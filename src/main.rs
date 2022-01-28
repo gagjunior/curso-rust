d2406d35ae8aa90e64e11ce320ca06f776baabce
Gilberto Junior <gagjunior@hotmail.com>
2022-01-28 13:32:06 -0300

    Iniciando input de dados

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use std::io;
+
+fn convert_to_int(data_input: &String) -> i32 {
+   let x = data_input.trim().parse::<i32>().unwrap();
+   x
+}
+
 fn main() {
-   let number1 = 30;
-   let number2 = 10;
+
+   //Ler numero 1
+   let mut number1 = String::new();
+   io::stdin().read_line(&mut number1).expect("Erro ao ler number1");   
+
+   //Ler numero 2
+   let mut number2 = String::new();
+   io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
 
    if number1 > number2 {
        println!("Numero 1({}) é maior que o numero 2({})", number1, number2);
