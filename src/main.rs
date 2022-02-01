3f67f1d607ad32bc6d8d1137838d0eca06cf6424
Gilberto Junior <gagjunior@hotmail.com>
2022-02-01 13:36:41 -0300

    Aula 11

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,23 @@ fn convert_to_int(data_input: &String) -> i32 {
 }
 
 fn main() {
+   println!("Digite o primeiro numero:");
 
-   //Ler numero 1
    let mut number1 = String::new();
-   io::stdin().read_line(&mut number1).expect("Erro ao ler number1");   
 
-   //Ler numero 2
+   io::stdin().read_line(&mut number1).expect("Falha ao ler entrada");
+
+   println!("Digite o segundo numero:");
+
    let mut number2 = String::new();
-   io::stdin().read_line(&mut number2).expect("Erro ao ler number2");
 
-   if number1 > number2 {
-       println!("Numero 1({}) é maior que o numero 2({})", number1, number2);
-    } 
-    else {
-       println!("Numero 1({}) é menor ou igual ao numero 2({})", number1, number2);
+   io::stdin()
+      .read_line(&mut number2)
+      .expect("Falha ao ler entrada");
+
+   if convert_to_int(&number1) > convert_to_int(&number2) {
+      println!("O numero {} eh maior que o numero {}", number1, number2);
+   } else {
+      println!("O numero {} eh menor ou igual ao numero {}", number1, number2);
    }
 }
